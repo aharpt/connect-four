a0591c3d7428262760dd8fd3ecc76721b94e484c
Aaron Harpt <a169h161@ku.edu>
2021-04-07 15:20:02 -0500

    Add initial code

---
+++ scripts.js
@@ -0,0 +1,46 @@
+/* Game Code */
+
+// global code
+let currentTurn = "";
+
+// Deciding who starts
+let decideTurn = () => {
+  let randomNumber = Math.floor(Math.random() * 2);
+  console.log(randomNumber);
+
+  if (randomNumber == 0) {
+    currentTurn = "Red";
+  } else {
+    currentTurn = "Black";
+  }
+
+  document.getElementById("current-turn").innerText = currentTurn;
+};
+
+// changing turn
+let changeTurn = (playerTurn) => {
+  document.getElementById("current-turn").innerText = playerTurn;
+};
+
+/* Event handlers */
+
+// Clear Buttons Handler is Clicked
+document.getElementById("clear-tokens-button").addEventListener("click", function() {
+  let tokens = document.querySelectorAll(".circle");
+
+  for (let i = 0; i < tokens.length; i++) {
+    tokens[i].style.backgroundColor = "white";
+  }
+
+  this.setAttribute("disabled", "true");
+  document.getElementById("start-game-button").removeAttribute("disabled");
+});
+
+// Start Game Button Click handler
+
+document.getElementById("start-game-button").addEventListener("click", function() {
+  this.setAttribute("disabled", "true");
+  document.getElementById("clear-tokens-button").removeAttribute("disabled");
+
+  decideTurn();
+});
